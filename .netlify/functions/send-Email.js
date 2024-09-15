9e76de1b513bd718544921f5e066afa4ff1b25bf
intercursosufrnceres <intercursosufrnceres@gmail.com>
2024-09-15 16:00:18 -0300

    Update send-Email.js

--- .netlify/functions/send-Email.js
+++ .netlify/functions/send-Email.js
@@ -2,51 +2,41 @@ const nodemailer = require('nodemailer');
 const { Buffer } = require('buffer');
 
 exports.handler = async function(event, context) {
-  try {
-    // Parse the request body
-    const { pdfBase64, email } = JSON.parse(event.body);
+  const { pdfBase64, email } = JSON.parse(event.body);
 
-    // Create a transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-      service: 'gmail', // Use seu serviço de e-mail
-      auth: {
-        user: process.env.EMAIL_USER, // Email do remetente
-        pass: process.env.EMAIL_PASS, // Senha do email do remetente
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // Use seu serviço de e-mail
+    auth: {
+      user: process.env.EMAIL_USER, // Email do remetente
+      pass: process.env.EMAIL_PASS, // Senha do email do remetente
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: 'intercursosufrnceres@gmail.com', // Definido diretamente aqui
+    subject: 'PDF da Inscrição',
+    text: 'Anexo está o PDF da sua inscrição.',
+    attachments: [
+      {
+        filename: 'inscricao_intercursos.pdf',
+        content: Buffer.from(pdfBase64, 'base64'),
+        encoding: 'base64',
       },
-    });
+    ],
+  };
 
-    // Define email options
-    const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: process.env.REACT_APP_EMAIL_RECIPIENT,
-      subject: 'PDF da Inscrição',
-      text: 'Anexo está o PDF da sua inscrição.',
-      attachments: [
-        {
-          filename: 'inscricao_intercursos.pdf',
-          content: Buffer.from(pdfBase64, 'base64'),
-          encoding: 'base64',
-        },
-      ],
-    };
-
-    // Send email
+  try {
     await transporter.sendMail(mailOptions);
-
-    // Return success response
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'E-mail enviado com sucesso!' }),
     };
   } catch (error) {
-    // Return error response with error details
+    console.error('Erro ao enviar e-mail:', error); // Adiciona um log do erro
     return {
       statusCode: 500,
-      body: JSON.stringify({ 
-        message: 'Falha ao enviar e-mail.',
-        error: error.message,
-        stack: error.stack
-      }),
+      body: JSON.stringify({ message: 'Falha ao enviar e-mail.', error: error.message }),
     };
   }
 };

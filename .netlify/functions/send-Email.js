8355ecea87b9422295a5223a4e9e57473828f459
intercursosufrnceres <intercursosufrnceres@gmail.com>
2024-09-15 15:37:13 -0300

    Update send-Email.js

--- .netlify/functions/send-Email.js
+++ .netlify/functions/send-Email.js
@@ -1,11 +1,8 @@
-// netlify/functions/send-email.js
 const nodemailer = require('nodemailer');
 const { Buffer } = require('buffer');
 
 exports.handler = async function(event, context) {
   const { pdfBase64, email } = JSON.parse(event.body);
-
-  console.log('Recebendo e-mail para:', email); // Log para depuração
 
   const transporter = nodemailer.createTransport({
     service: 'gmail', // Use seu serviço de e-mail
@@ -17,7 +14,7 @@ exports.handler = async function(event, context) {
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
-    to: process.env.REACT_APP_EMAIL_RECIPIENT,,
+    to: email,
     subject: 'PDF da Inscrição',
     text: 'Anexo está o PDF da sua inscrição.',
     attachments: [
@@ -31,13 +28,11 @@ exports.handler = async function(event, context) {
 
   try {
     await transporter.sendMail(mailOptions);
-    console.log('E-mail enviado com sucesso!'); // Log para depuração
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'E-mail enviado com sucesso!' }),
     };
   } catch (error) {
-    console.error('Erro ao enviar e-mail:', error); // Log para depuração
     return {
       statusCode: 500,
       body: JSON.stringify({ message: 'Falha ao enviar e-mail.' }),

a2b5d21454a5e7fa6ae9db09832d85c05eccaf50
intercursosufrnceres <intercursosufrnceres@gmail.com>
2024-09-15 16:29:58 -0300

    Update send-Email.js

--- .netlify/functions/send-Email.js
+++ .netlify/functions/send-Email.js
@@ -3,10 +3,15 @@ const { Buffer } = require('buffer');
 
 exports.handler = async function(event, context) {
   console.log('Request received');
-  
+
   try {
     // Verificar o corpo da solicitação
     const { pdfBase64, email } = JSON.parse(event.body);
+    
+    if (!pdfBase64 || !email) {
+      throw new Error('pdfBase64 ou email ausente no corpo da solicitação');
+    }
+
     console.log('PDF Base64:', pdfBase64);
     console.log('Recipient Email:', email);
 
@@ -22,7 +27,7 @@ exports.handler = async function(event, context) {
     // Opções do e-mail
     const mailOptions = {
       from: process.env.EMAIL_USER,
-      to: 'intercursosufrnceres@gmail.com',
+      to: email, // Enviar para o e-mail fornecido no corpo da solicitação
       subject: 'PDF da Inscrição',
       text: 'Anexo está o PDF da sua inscrição.',
       attachments: [

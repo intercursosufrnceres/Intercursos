659600eb63bdd641b03a0ca54f9aa5c31c1672ba
intercursosufrnceres <intercursosufrnceres@gmail.com>
2024-09-15 16:14:30 -0300

    Update send-Email.js

--- .netlify/functions/send-Email.js
+++ .netlify/functions/send-Email.js
@@ -2,41 +2,52 @@ const nodemailer = require('nodemailer');
 const { Buffer } = require('buffer');
 
 exports.handler = async function(event, context) {
-  const { pdfBase64 } = JSON.parse(event.body);
-
-  const transporter = nodemailer.createTransport({
-    service: 'gmail', // Use seu serviço de e-mail
-    auth: {
-      user: process.env.EMAIL_USER, // Email do remetente
-      pass: process.env.EMAIL_PASS, // Senha do email do remetente
-    },
-  });
-
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: 'intercursosufrnceres@gmail.com', // Definido diretamente aqui
-    subject: 'PDF da Inscrição',
-    text: 'Anexo está o PDF da sua inscrição.',
-    attachments: [
-      {
-        filename: 'inscricao_intercursos.pdf',
-        content: Buffer.from(pdfBase64, 'base64'),
-        encoding: 'base64',
-      },
-    ],
-  };
-
+  console.log('Request received');
+  
   try {
+    // Verificar o corpo da solicitação
+    const { pdfBase64, email } = JSON.parse(event.body);
+    console.log('PDF Base64:', pdfBase64);
+    console.log('Recipient Email:', email);
+
+    // Configurar o transportador
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    // Opções do e-mail
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: 'intercursosufrnceres@gmail.com',
+      subject: 'PDF da Inscrição',
+      text: 'Anexo está o PDF da sua inscrição.',
+      attachments: [
+        {
+          filename: 'inscricao_intercursos.pdf',
+          content: Buffer.from(pdfBase64, 'base64'),
+          encoding: 'base64',
+        },
+      ],
+    };
+
+    console.log('Mail Options:', mailOptions);
+
+    // Enviar e-mail
     await transporter.sendMail(mailOptions);
+    console.log('E-mail enviado com sucesso');
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'E-mail enviado com sucesso!' }),
     };
   } catch (error) {
-    console.error('Erro ao enviar e-mail:', error); // Adiciona um log do erro
+    console.error('Erro ao enviar e-mail:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Falha ao enviar e-mail back.', error: error.message }), // Inclui o erro no corpo da resposta
+      body: JSON.stringify({ message: 'Falha ao enviar e-mail.', error: error.message }),
     };
   }
 };
